28cf3c0f362ca641d3a1fac2456e0461f0d62f92
John Appleseed <email@example.com>
2018-02-23 17:11:52 -0500

    hw2

--- 430/query.sql
+++ 430/query.sql
@@ -1,7 +1,7 @@
 with test as 
 	(select studentnum, count(schedulenum) as count
 	  from taking
-	  where semester = "SPRING"
+	  where semester = 'SPRING'
 	  group by studentnum)
 	select name, max(newcount)
 	from student,test
