1418306bdafee4863a489f5ceeef2c85d8e9eae4
John Appleseed <email@example.com>
2018-02-23 14:11:06 -0500

    hw2

--- 430/query.sql
+++ 430/query.sql
@@ -1,6 +1,6 @@
 select name
 from student
 where EXISTS (select distinct name
-				from student inner join taking on (student.studentnum = taking.studentnum) 
+				from student as stud inner join taking on (stud.studentnum = taking.studentnum) 
 				inner join class on (class.schedulenum = taking.schedulenum)
-				where department != 'CMPSCI' and num != 430);
+				where department != 'CMPSCI' and num != 430 and stud.studentnum = student.studentnum);
