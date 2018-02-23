72749806c10ffb00bc8dc8b01e486948c8d1cbb3
John Appleseed <email@example.com>
2018-02-23 01:15:43 -0500

    hw2

--- 430/query.sql
+++ 430/query.sql
@@ -1,3 +1,3 @@
 select name
-from student inner join taking on(studentnum)
-where department = 'CMPSCI' and num = '430';
+from student inner join taking on(studentnum) inner join class on (schedulenum)
+where department = 'CMPSCI' and num = 430;
