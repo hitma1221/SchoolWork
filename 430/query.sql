e0211d75691d45f501ee951ca68c57b00a47f8fb
John Appleseed <email@example.com>
2018-02-23 15:53:01 -0500

    hw2

--- 430/query.sql
+++ 430/query.sql
@@ -2,5 +2,6 @@ select name
 from student
 where studentnum = (select studentnum, count(schedulenum)
 					from taking
-					where count(schedulenum) = max(count(schedulenum)));
+					where count(schedulenum) = max(count(schedulenum))
+					group by studentnum);
 						
