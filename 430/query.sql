b00f4f6ff965e6e1a799143296b61bac21f8264f
John Appleseed <email@example.com>
2018-02-23 14:00:04 -0500

    hw2

--- 430/query.sql
+++ 430/query.sql
@@ -1,9 +1,6 @@
-select distinct name, standing
-from student inner join taking on (student.studentnum = taking.studentnum)
-where schedulenum in ( select schedulenum
-					 from student inner join taking
-					 on (student.studentnum = taking.studentnum)
-						Intersect
-					select schedulenum
-					from teaches inner join Instructor on(teaches.name = Instructor.name)
-					where teaches.name = 'Linda Null' or teaches.name = 'Jeremy Blum');
+select name
+from student
+where EXISTS (select distinct name
+				from student inner join taking on (student.studentnum = taking.studentnum) 
+				inner join class on (class.schedulenum = taking.schedulenum)
+				where department != 'CMPSCI' and num != 430);
