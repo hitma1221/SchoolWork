e787e007a60aa525ca3b1db58b197722dc72b64b
John Appleseed <email@example.com>
2018-02-23 14:46:55 -0500

    hw2

--- 430/query.sql
+++ 430/query.sql
@@ -1,4 +1,14 @@
-select name
+SELECT name
+				FROM Student stud
+				where student.studentnum = stud.studentnum and 
+				studentnum in
+				(select studentnum
+					from taking
+					where schedulenum in 
+					(select schedulenum
+					 from class
+					 where num != 430 and department != 'CMPSCI'))
+/*select name
 from student
 where EXISTS (SELECT name
 				FROM Student stud
@@ -9,4 +19,4 @@ where EXISTS (SELECT name
 					where schedulenum in 
 					(select schedulenum
 					 from class
-					 where num != 430 and department != 'CMPSCI')));
+					 where num != 430 and department != 'CMPSCI')));*/
