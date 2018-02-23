3b185103cec7119eb767a306d564364de5839224
John Appleseed <email@example.com>
2018-02-23 00:50:48 -0500

    hw2

--- 430/query.sql
+++ 430/query.sql
@@ -1,7 +1,6 @@
 select schedulenum, semester
 from taking
-where schedulenum in (
-						select schedulenum
-						from class
-						where num = 430 and department = 'CMPSCI')
+where schedulenum in (select schedulenum
+					  from class
+					  where num = 430 and department = 'CMPSCI')
 group by avg(grade);
