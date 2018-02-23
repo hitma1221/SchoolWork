6980badf5e8add079506490c0af042d447ee7115
John Appleseed <email@example.com>
2018-02-23 00:10:02 -0500

    hw2

--- 430/query.sql
+++ 430/query.sql
@@ -1,7 +1,19 @@
+/* problem b
 SELECT name
 FROM Student
 where studentnum in (select studentnum
 					from taking
 					where schedulenum in (select schedulenum
 										 from class
-										 where num = 430 and department = 'CMPSCI'));
+										 where num = 430 and department = 'CMPSCI' and year = '2018'));
+*/
+select name, standing
+from student
+MINUS
+select name, standing
+from student
+where studentnum in (select studentnum
+					from taking
+					where schedulenum in (select schedulenum
+						 				  from teaches
+						 				  where name != 'Linda Null' or name != 'Jeremy Blum')
