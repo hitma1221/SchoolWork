3fd371c009d03c33ba2b202de779a74f1242d287
John Appleseed <email@example.com>
2018-02-22 22:52:28 -0500

    hw2

--- 430/hw2.sql
+++ 430/hw2.sql
@@ -44,9 +44,30 @@ CREATE TABLE Taking(
 	FOREIGN KEY(schedulenum, semester) REFERENCES Class
 );
 
-INSERT INTO Instructor(name, department, office)
-VALUES('Jeremy Blum', 'CMPSCI', 'W255'),
-('Sukmoon Chang', 'CMPSCI', 'W255');
+INSERT INTO Instructor(name, department, office) VALUES('Jeremy Blum', 'CMPSCI', 'W255');
+INSERT INTO Instructor(name, department, office) VALUES('Sukmoon Chang', 'CMPSCI', 'W255');
+INSERT INTO Instructor(name, department, office) VALUES('Linda Null', 'CMPSCI', 'W255');
+INSERT INTO Instructor(name, department, office) VALUES('Hyuntae Na', 'CMPSCI', 'W255');
+INSERT INTO Instructor(name, department, office) VALUES('Justin Keller', 'Math', 'W255');
+
+INSERT INTO Class(schedulenum, semester, department, num, days, time, place, enrollment) 
+VALUES(123, 'Spring', 'CMPSCI', 430, 'T/Th', '1:35', 'EAB', 'Enrolled');
+INSERT INTO Class(schedulenum, semester, department, num, days, time, place, enrollment) 
+VALUES(124, 'Spring', 'MATH', 455, 'T/Th', '9:00', 'Olmstead', 'Enrolled');
+INSERT INTO Class(schedulenum, semester, department, num, days, time, place, enrollment) 
+VALUES(125, 'Fall', 'BIO', 100, 'M/W/F', '1:25', 'EAB', ' Not Enrolled');
+INSERT INTO Class(schedulenum, semester, department, num, days, time, place, enrollment) 
+VALUES(126, 'Spring', 'CMPSCI', 463, 'W', '6:00', 'SEC', 'Not Enrolled');
+INSERT INTO Class(schedulenum, semester, department, num, days, time, place, enrollment) 
+VALUES(127, 'Spring', 'CMPSCI', 460, 'W', '6:00', 'Olmstead', 'Enrolled');
+
+INSERT INTO Teaches(name, schedulenum, semester) VALUES('Jeremy Blum', 123, 'Spring');
+INSERT INTO Teaches(name, schedulenum, semester) VALUES('Jeremy Blum', 126, 'Spring');
+INSERT INTO Teaches(name, schedulenum, semester) VALUES('Sukmoon Chang', 127, 'Spring');
+INSERT INTO Teaches(name, schedulenum, semester) VALUES('Linda Null', 125, 'Fall');
+INSERT INTO Teaches(name, schedulenum, semester) VALUES('Justin Keller', 124, 'Spring');
+
+
 
 SELECT name
 FROM Instructor;
