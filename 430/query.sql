a203aa373dfcb032855c195f4b1f6824cc1e9a6c
Josh <hitma1221@live.com>
2018-03-22 16:14:42 -0400

    test

--- 430/query.sql
+++ 430/query.sql
@@ -1,9 +1,7 @@
 delete from student
 	where EXISTS
 	(select *
-		from student inner join taking using(studentnum) innerSql
-		where innerSql.schedulenum = (select schedulenum
-										from class
-										where department = 'CMPSC' and num = 463)
-		and student.studentnum = innerSql.studentnum
+		from student inner join taking using(studentnum),
+		class
+		where department = 'CMPSC' and num = '430'
 	);
