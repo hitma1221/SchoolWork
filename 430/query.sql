c81be3dabf59a14005c7c7696967861f33d7ac5c
Josh <hitma1221@live.com>
2018-03-22 15:50:51 -0400

    test

--- 430/query.sql
+++ 430/query.sql
@@ -1,5 +1,5 @@
 create view report_card as
 	(select info.name, info.semester, info.department, schedulenum, grade
-		from studnet inner join taking using(studentnum) as info,
+		from studnet inner join taking using(studentnum) info,
 		class
 		);
