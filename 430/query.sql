627ff5353f748653dd4110d397266fbc13c29b93
Josh <hitma1221@live.com>
2018-03-22 15:55:41 -0400

    test

--- 430/query.sql
+++ 430/query.sql
@@ -1,6 +1,6 @@
 create view report_card as
 	(select name, semester, department, schedulenum, grade
-		from student inner join taking using(studentnum),
+		from student inner join taking using(studentnum)
 		inner join class using (schedulenum)
 		where semester = "F18"
 		);
